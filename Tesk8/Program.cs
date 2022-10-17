af3cd5540095bf8014ffaa2042d118f858f05c49
Natasha <khanina.nata@mail.ru>
2022-10-17 19:15:34 +0300

    Задача8 написанна

---
+++ Tesk8/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число , ");
+int N = int.Parse(Console.ReadLine()!);
+for (int x = 2; x < N + 1; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
