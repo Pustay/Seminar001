962226f42de431bc43aa1b61ef1fb1a716884ad4
Natasha <khanina.nata@mail.ru>
2022-10-17 18:21:15 +0300

    Задача2 написанна

--- Tesk2/Program.cs
+++ Tesk2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два
+// числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число 1, ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2, ");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine($"{a} больше {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"{a} меньше {b}");
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+}
+Console.ReadKey();
